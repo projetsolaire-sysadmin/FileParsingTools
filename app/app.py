f4b5792b67b07b93e13e87a656b0655792c59534
Maarten Elgar <maartenelgar12@gmail.com>
2023-11-01 17:39:37 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -91,14 +91,14 @@ def post():
     if request.method == 'POST':
         f = request.files['file']
         if secure_filename(f.filename)[-4:]==".csv":
-                print("here at post")
+            print("here at post")
             f.save(os.path.join(os.path.join(app.root_path, app.config['UPLOAD_FOLDER']), f.filename))
             app.config['filename']=f.filename
             if do_work():
-                    print("here at post")
+                print("here at post")
                 return render_template('base+download.html')
             else:
-                          return render_template('base+download.html')
+                return render_template('base+download.html')
 
 
         else:
